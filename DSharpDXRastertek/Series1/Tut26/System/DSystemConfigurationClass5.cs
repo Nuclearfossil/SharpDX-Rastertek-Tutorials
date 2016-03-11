0ecd8e80b7b080f8949269510dc72f26865dbf6b
Dan Kalinich <dan6040mcp@hotmail.com>
2016-03-11 11:00:32 -0500

    Commit Tutorial 26 27 abd nives Shader files nad modified Path

--- DSharpDXRastertek/Series1/Tut26/System/DSystemConfigurationClass5.cs
+++ DSharpDXRastertek/Series1/Tut26/System/DSystemConfigurationClass5.cs
@@ -50,7 +50,7 @@ namespace DSharpDXRastertek.Tut26.System
             ScreenNear = 0.1f;
             BorderStyle = FormBorderStyle.None;
 
-            ShaderFilePath = @"Series1\Tut26\Externals\Shaders\";
+            ShaderFilePath = @"Series1\Tut26\Shaders\";
             DataFilePath = @"Externals\Data\";
             ModelFilePath = @"Externals\Models\";
         }
