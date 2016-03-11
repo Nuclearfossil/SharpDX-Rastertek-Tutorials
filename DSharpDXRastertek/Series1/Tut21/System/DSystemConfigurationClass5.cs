24bd0f4f22c6c2f3b921112919c47bd029ef4aa0
Dan Kalinich <dan6040mcp@hotmail.com>
2016-03-11 10:36:04 -0500

    Moved Tutorial 21 Shader files from Library project to WinForms  Project and modified Shader Path.

--- DSharpDXRastertek/Series1/Tut21/System/DSystemConfigurationClass5.cs
+++ DSharpDXRastertek/Series1/Tut21/System/DSystemConfigurationClass5.cs
@@ -50,7 +50,7 @@ namespace DSharpDXRastertek.Tut21.System
             ScreenNear = 0.1f;
             BorderStyle = FormBorderStyle.None;
 
-            ShaderFilePath = @"Series1\Tut21\Externals\Shaders\";
+            ShaderFilePath = @"Series1\Tut21\Shaders\";
             DataFilePath = @"Externals\Data\";
             ModelFilePath = @"Externals\Models\";
         }
