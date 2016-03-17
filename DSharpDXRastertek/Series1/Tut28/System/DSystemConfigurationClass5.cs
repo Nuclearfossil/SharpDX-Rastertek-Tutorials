b542fae8844264bbb22e5d42e08f27f42297be9e
Dan Kalinich <dan6040mcp@hotmail.com>
2016-03-16 20:58:20 -0400

    Moved shader files from Tutorial 28 class library prohect to Winforms one.

--- DSharpDXRastertek/Series1/Tut28/System/DSystemConfigurationClass5.cs
+++ DSharpDXRastertek/Series1/Tut28/System/DSystemConfigurationClass5.cs
@@ -51,7 +51,7 @@ namespace DSharpDXRastertek.Tut28.System
             ScreenNear = 0.1f;
             BorderStyle = FormBorderStyle.None;
 
-            ShaderFilePath = @"Series1\Tut28\Externals\Shaders\";
+            ShaderFilePath = @"Series1\Tut28\Shaders\";
             DataFilePath = @"Externals\Data\";
             ModelFilePath = @"Externals\Models\";
         }
