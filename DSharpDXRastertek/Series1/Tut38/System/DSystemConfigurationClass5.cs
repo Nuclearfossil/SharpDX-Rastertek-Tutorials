93640e1b60347c3716a6ada040f210be42d4cfdf
Dan Kalinich <dan6040mcp@hotmail.com>
2016-03-17 14:01:25 -0400

    Modified path and moved Shader files for Tutorial 38

--- DSharpDXRastertek/Series1/Tut38/System/DSystemConfigurationClass5.cs
+++ DSharpDXRastertek/Series1/Tut38/System/DSystemConfigurationClass5.cs
@@ -50,7 +50,7 @@ namespace DSharpDXRastertek.Tut38.System
             ScreenNear = 0.1f;
             BorderStyle = FormBorderStyle.None;
 
-            ShaderFilePath = @"Series1\Tut38\Externals\Shaders\";
+            ShaderFilePath = @"Series1\Tut38\Shaders\";
         }
     }
 }
